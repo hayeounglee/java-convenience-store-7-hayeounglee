4fcacbae1d98e8426371998f6ad974b150f87db9
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-11-07 21:53:31 +0900

    feat: 처음 입력할 때 빈값일 경우 예외처리하는 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,21 +1,38 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import store.validator.CommonValidator;
 
 public class InputView {
+    private final CommonValidator commonInputValidator = new CommonValidator();
+
     public String getProductAndPrice() {
         System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
-        return Console.readLine();
+        return getInput();
     }
 
-    public String getYesOrNo() {
-        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
-        return Console.readLine();
+    public boolean getMembershipDiscountOrNot() {
+        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        return getYesOrNo();
     }
 
-    public String getAdditionalPurchase() {
+    public boolean getAdditionalPurchase() {
         System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
-        return Console.readLine();
+        return getYesOrNo();
     }
 
+    private String getInput() {
+        String input = Console.readLine();
+        commonInputValidator.validateEmpty(input);
+        return input;
+    }
+
+    private boolean getYesOrNo() {
+        String input = Console.readLine();
+        commonInputValidator.validateEmpty(input);
+        if (input == "Y") {
+            return true;
+        }
+        return false;
+    }
 }
