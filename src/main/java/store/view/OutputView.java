f4376ed3508d9d840919d7a7c0a6d2df10803712
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-11-08 01:12:40 +0900

    feat: 편의점 제품과 금액 정보 출력하는 기능 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -3,6 +3,7 @@ package store.view;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 public class OutputView {
     public void printAddMessage() {
@@ -28,9 +29,45 @@ public class OutputView {
 
         while ((product = reader.readLine()) != null) {
             String[] str = product.split(",");
-            System.out.printf("- %s %s원 %s개 %s", str[0], String.format(str[1]), str[2], str[3]); //null은 재고 없음으로 하기
+            System.out.printf("- %s %s원 %s개 %s", str[0], String.format("%,d", Integer.parseInt(str[1])), str[2], str[3]); //null은 재고 없음으로 하기
             System.out.println();
         }
         reader.close();
+    }
+
+
+    public void printReceipt(List<String> result) throws IOException {
+        printPurchaseProduct();
+        printAmountInfo(result);
+    }
+
+    private void printPurchaseProduct() throws IOException {
+        System.out.println("==============W 편의점================\n" + "상품명\t\t수량\t금액");
+
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/receipt.md")); //java 파일 경로 입력방법
+        String product;
+        reader.readLine();
+
+        while ((product = reader.readLine()) != null) {
+            String[] str = product.split(",");
+            System.out.printf("%s\t\t%s\t%s", str[0], str[1], String.format("%,d", Integer.parseInt(str[2]))); //null은 재고 없음으로 하기
+            System.out.println();
+        }
+        reader.close();
+
+        System.out.println("=============증\t정===============");
+
+    }
+
+    private void printAmountInfo(List<String> result) {
+        System.out.println("====================================");
+        System.out.printf("총구매액\t\t%s\t%s", result.get(0), String.format("%,d", Integer.parseInt(result.get(1))));
+        System.out.println();
+        System.out.printf("행사할인\t\t%s", String.format("%,d", Integer.parseInt(result.get(2))));
+        System.out.println();
+        System.out.printf("멤버십할인\t\t%s", String.format("%,d", Integer.parseInt(result.get(3))));
+        System.out.println();
+        System.out.printf("내실돈\t\t%s", String.format("%,d", Integer.parseInt(result.get(4))));
+
     }
 }
