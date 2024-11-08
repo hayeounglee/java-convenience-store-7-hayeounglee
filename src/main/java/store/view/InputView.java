83417fe4c5cf55046f78b5704c95c6da3ff800d6
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-11-08 15:54:08 +0900

    feat: 1개 더 무료로 받는지 입력받는 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,6 +1,7 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import store.model.Product;
 import store.validator.CommonValidator;
 
 public class InputView {
@@ -18,6 +19,11 @@ public class InputView {
 
     public boolean getAdditionalPurchase() {
         System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+        return getYesOrNo();
+    }
+
+    public boolean getOneMoreFree(Product product) {
+        System.out.printf("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)", product.getName());
         return getYesOrNo();
     }
 
