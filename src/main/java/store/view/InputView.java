387e1fd11b65a1680efed88113a256fc021f2166
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-11-08 18:25:01 +0900

    feat: 추가 입력받는 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -27,6 +27,12 @@ public class InputView {
         return getYesOrNo();
     }
 
+    public boolean getPurchaseOrNot(String name, int count) {
+        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)", name, count);
+        System.out.println();
+        return getYesOrNo();
+    }
+
     private String getInput() {
         String input = Console.readLine();
         commonInputValidator.validateEmpty(input);
