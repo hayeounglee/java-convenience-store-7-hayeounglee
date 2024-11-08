ecf5ab66e4eb4350e6575558cfa431515f68cc83
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-11-08 22:21:59 +0900

    refactor: 상황에 따라 공백 줄 추가로 수정

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -14,13 +14,11 @@ public class InputView {
 
     public boolean getMembershipDiscountOrNot() {
         System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
-        System.out.println();
         return getYesOrNo();
     }
 
     public boolean getAdditionalPurchase() {
         System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
-        System.out.println();
         return getYesOrNo();
     }
 
