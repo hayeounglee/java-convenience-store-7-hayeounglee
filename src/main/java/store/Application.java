db3bce8f75df9b4f56f6f27b54ba5a1bb4be24f8
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-11-09 16:40:36 +0900

    feat: 컨트롤러 실행시키는 기능 추가

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,14 @@
 package store;
 
+import store.controller.StoreController;
+import store.view.InputView;
+import store.view.OutputView;
+
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IOException {
+        StoreController storeController = new StoreController(new InputView(), new OutputView());
+        storeController.run();
     }
 }
