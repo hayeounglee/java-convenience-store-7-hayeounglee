f4b0539da12f2d5038191b3b4a6ebd846ec7d383
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-11-10 21:53:37 +0900

    refactor: 함수 분리 과정에서 오류가 발생하여 분리 전으로 수정

--- src/main/java/store/model/Product.java
+++ src/main/java/store/model/Product.java
@@ -24,69 +24,22 @@ public class Product {
     }
 
     private String[] validate(String product) throws IOException {
-        validateForm(product);
-
-        String[] oneProduct = extractOneProduct(product);
-
-        validateOneProductForm(oneProduct);
-        validateName(oneProduct);
-        validateQuantity(oneProduct);
-
-        return oneProduct;
-    }
-
-    private String[] extractOneProduct(String product) {
-        String inputProduct = product.substring(1, product.length() - 1); // 깔끔하게 파악할 수 있게
-        return inputProduct.split("-", -1);
-    }
-
-    private void validateForm(String product) {
         if (product.isBlank() | product.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
         }
         if (!(product.charAt(0) == '[' && product.charAt(product.length() - 1) == ']' && product.contains("-"))) { //"" vs ''의 차이????? String vs char
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
         }
-    }
 
-    private void validateOneProductForm(String[] oneProduct) {
+        product = product.substring(1, product.length() - 1); // 깔끔하게 파악할 수 있게
+        String[] oneProduct = product.split("-", -1);
+
         for (String one : oneProduct) {
             if (one.isBlank() | one.isEmpty()) {
                 throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
             }
         }
-    }
 
-    private void validateName(String[] oneProduct) throws IOException {
-        if (!isExistProduct(oneProduct[0])) {
-            throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
-        }
-    }
-
-    private boolean isExistProduct(String productName) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/products.md"))) {
-            String food;
-            reader.readLine(); // Skip header line
-            while ((food = reader.readLine()) != null) {
-                if (updatePrice(food, productName)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private boolean updatePrice(String food, String productNAme) {
-        String foodName = food.split(",")[0];
-        String foodPrice = food.split(",")[1];
-        if (foodName.equals(productNAme)) {
-            price = Integer.parseInt(foodPrice);
-            return true;
-        }
-        return false;
-    }
-
-    private void validateQuantity(String[] oneProduct) throws IOException {
         try {
             Integer.parseInt(oneProduct[1]);
         } catch (IllegalArgumentException e) {
@@ -97,43 +50,62 @@ public class Product {
             throw new IllegalArgumentException("[ERROR] 수량은 1이상의 숫자를 입력해야 합니다. 다시 입력해 주세요.");
         }
 
+        if (!isExistProduct(oneProduct[0])) {
+            throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
+        }
+
         if (isStockLack(oneProduct)) {
             throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
         }
+        return oneProduct;
+    }
+
+    private boolean isExistProduct(String productName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/products.md")); //java 파일 경로 입력방법
+        String food;
+        reader.readLine();
+
+        while ((food = reader.readLine()) != null) {
+            String foodName = food.split(",")[0];
+            String foodPrice = food.split(",")[1];
+
+            if (foodName.equals(productName)) {
+                price = Integer.parseInt(foodPrice);
+                reader.close();
+                return true;
+            }
+        }
+        reader.close();
+        return false;
     }
 
     private boolean isStockLack(String[] productInfo) throws IOException {
-        findProductStock(productInfo);
-        return promotionStockCount + normalStockCount < Integer.parseInt(productInfo[1]);
-    }
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/products.md")); //java 파일 경로 입력방법
+        String food;
+        reader.readLine();
 
-    private void findProductStock(String[] productInfo) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/products.md"))) {
-            String food;
+        while ((food = reader.readLine()) != null) {
+            String foodName = food.split(",")[0];
+            String foodQuantity = food.split(",")[2];
+            String foodPromotion = food.split(",")[3];
 
-            reader.readLine();
-            while ((food = reader.readLine()) != null) {
-                String[] foodInfo = food.split(",");
-                updateStock(foodInfo, productInfo);
+            if (foodName.equals(productInfo[0]) & !foodPromotion.equals("null")) {
+                promotionCount = getPromotionCount(foodPromotion);
+                if (promotionCount != 0) {
+                    promotion = foodPromotion;
+                    promotionStockCount += Integer.parseInt(foodQuantity);
+                }
             }
-        }
-    }
-
-    private void updateStock(String[] foodInfo, String[] productInfo) throws IOException {
-        String foodName = foodInfo[0];
-        String foodQuantity = foodInfo[2];
-        String foodPromotion = foodInfo[3];
-
-        if (foodName.equals(productInfo[0])) {
-            if (foodPromotion.equals("null")) {
+            if (foodName.equals(productInfo[0]) & foodPromotion.equals("null")) {
                 normalStockCount += Integer.parseInt(foodQuantity);
-            }//마지막에 수행하는 걸로 수정하는 거 어떤가 싶음
-            promotionCount = getPromotionCount(foodPromotion);
-            if (promotionCount != 0) {
-                promotion = foodPromotion;
-                promotionStockCount += Integer.parseInt(foodQuantity);
             }
         }
+        if (promotionStockCount + normalStockCount < Integer.parseInt(productInfo[1])) {
+            reader.close();
+            return true;
+        }
+        reader.close();
+        return false;
     }
 
     public boolean isAvailableOnlyPromotion() {
@@ -176,28 +148,20 @@ public class Product {
     }
 
     public int getPromotionCount(String promotionName) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/promotions.md"))) {
-            String readPromotion;
-            int countPromotion = 0;
+        // if (promotion.equals("null")) return 0;
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/promotions.md")); //java 파일 경로 입력방법
+        String readPromotion;
 
-            reader.readLine();
-            while ((readPromotion = reader.readLine()) != null) {
-                countPromotion = getValidatePromotion(readPromotion.split(","), promotionName);
+        reader.readLine();
+
+        while ((readPromotion = reader.readLine()) != null) {
+            String[] promotionInfo = readPromotion.split(",");
+            if (promotionInfo[0].equals(promotionName) & isValidateDate(promotionInfo[3], promotionInfo[4])) {
+                reader.close();
+                return Integer.parseInt(promotionInfo[1]) + Integer.parseInt(promotionInfo[2]); //각각 parse해야 하나?
             }
-            return countPromotion;
         }
-    }
-
-    private int getValidatePromotion(String[] promotionInfo, String promotionName) {
-        String name = promotionInfo[0];
-        int buy = Integer.parseInt(promotionInfo[1]);
-        int get = Integer.parseInt(promotionInfo[2]);
-        String startDate = promotionInfo[3];
-        String endDate = promotionInfo[4];
-
-        if (name.equals(promotionName) & isValidateDate(startDate, endDate)) {
-            return buy + get;
-        }
+        reader.close();
         return 0;
     }
 
@@ -237,4 +201,3 @@ public class Product {
         return promotionStockCount;
     }
 }
-
