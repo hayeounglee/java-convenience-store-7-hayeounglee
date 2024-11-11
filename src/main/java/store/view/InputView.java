d918c384d372dd13f77addc3ef91c39f79491cc8
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-11-11 23:45:17 +0900

    refactor: 줄 수정

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -12,28 +12,33 @@ public class InputView {
     }
 
     public String getProductAndPrice() {
-        System.out.println("\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        System.out.println();
+        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
         return getInput();
     }
 
     public boolean getMembershipDiscountOrNot() {
-        System.out.println("\n멤버십 할인을 받으시겠습니까? (Y/N)");
+        System.out.println();
+        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
         return getYesOrNo();
     }
 
     public boolean getAdditionalPurchase() {
-        System.out.println("\n감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+        System.out.println();
+        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
         return getYesOrNo();
     }
 
     public boolean getOneMoreFree(Product product) {
-        System.out.printf("\n현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)", product.getName(), product.getPromotionGetCount());
+        System.out.println();
+        System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)", product.getName(), product.getPromotionGetCount());
         System.out.println();
         return getYesOrNo();
     }
 
     public boolean getPurchaseOrNot(String name, int count) {
-        System.out.printf("\n현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)", name, count);
+        System.out.println();
+        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)", name, count);
         System.out.println();
         return getYesOrNo();
     }
